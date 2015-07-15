a9e5bfebc4a821820a4edcf534bfb9bdde087cb2
zbourn <zjasonbourn@yahoo.in>
2015-07-16 03:13:02 +0530

    Commit

---
+++ zui5_carrier_rate_approval/WebContent/views/App.view.js
@@ -0,0 +1,36 @@
+sap.ui.jsview("nad.views.App", {
+
+	getControllerName : function() {
+		return "nad.views.App";
+	},
+
+	createContent : function(oController) {
+
+		// to avoid scroll bars on desktop the root view must be set to block
+		// display
+		this.setDisplayBlock(true);
+
+		// create app
+		this.app = new sap.m.SplitApp();
+
+		this.app.addStyleClass("splitAppStyle");
+		this.app.setHomeIcon({
+			'phone' : 'i/icon/phone-icon.png',
+			'phone@2' : 'i/icon/phone-retina.png',
+			'tablet' : 'i/icon/tablet-icon.png',
+			'tablet@2' : 'i/icon/tablet-retina.png',
+			'icon' : 'i/icon/desktop.ico'
+		});
+
+		// load the master page
+		var master = sap.ui.xmlview("Login", "nad.views.Login");
+		master.getController().nav = this.getController();
+		this.app.addPage(master, true);
+
+		// load the empty page
+		var detail = sap.ui.xmlview("Empty", "nad.views.Empty");
+		this.app.addPage(detail, false);
+
+		return this.app;
+	}
+});
